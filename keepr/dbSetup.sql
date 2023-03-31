a376d8a5ffa63df5c54b1674377571742d829756
JoeCalvi <joecalvi@me.com>
2023-03-31 10:56:54 -0600

    keeps table added, files started

--- keepr/dbSetup.sql
+++ keepr/dbSetup.sql
@@ -4,5 +4,19 @@ CREATE TABLE IF NOT EXISTS accounts(
   updatedAt DATETIME DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT 'Last Update',
   name varchar(255) COMMENT 'User Name',
   email varchar(255) COMMENT 'User Email',
-  picture varchar(255) COMMENT 'User Picture'
+  picture varchar(500) COMMENT 'User Picture',
+  coverImg VARCHAR(500) COMMENT 'User Cover Image'
 ) default charset utf8 COMMENT '';
+
+CREATE TABLE IF NOT EXISTS keeps(
+  id INT AUTO_INCREMENT NOT NULL PRIMARY KEY COMMENT 'Keep Id',
+  creatorId VARCHAR(255) NOT NULL COMMENT 'Keep Creator Id',
+  name VARCHAR(50) NOT NULL COMMENT 'Keep Name',
+  description VARCHAR(150) NOT NULL COMMENT 'Keep Description',
+  img VARCHAR(500) NOT NULL COMMENT 'Keep ImgUrl',
+  views INT NOT NULL DEFAULT 0 COMMENT 'Total Keep Views',
+
+  FOREIGN KEY (creatorId) REFERENCES accounts(id) ON DELETE CASCADE
+) default charset utf8 COMMENT '';
+
+DROP TABLE IF EXISTS keeps;
