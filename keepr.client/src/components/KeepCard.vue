4e8ec9a1bb4f98afef89f9c050ef2d37b413a62e
JoeCalvi <joecalvi@me.com>
2023-04-03 15:31:19 -0600

    workin on account page

--- keepr.client/src/components/KeepCard.vue
+++ keepr.client/src/components/KeepCard.vue
@@ -6,13 +6,24 @@
             <span>
                 {{ keep?.name }}
             </span>
-            <img class="creator-picture rounded-circle" :src="keep?.creator.picture" :alt="keep?.creator.picture">
+            <img v-if="keep?.creatorId != account?.id" class="creator-picture rounded-circle" :src="keep?.creator.picture"
+                :alt="keep?.creator.picture">
+            <div v-if="keep?.creatorId == account?.id" class="dropstart">
+                <button class="btn dropdown-toggle" type="button" data-bs-toggle="dropdown" aria-expanded="false">
+                    <span class="edit-keep">...</span>
+                </button>
+                <ul class="dropdown-menu create-options" style="background-color: #DED6E9;">
+                    <li><button class="dropdown-item" type="button">edit keep</button></li>
+                    <li><button class="dropdown-item" type="button">delete keep</button></li>
+                </ul>
+            </div>
         </div>
     </div>
 </template>
 
 
 <script>
+import { computed } from '@vue/reactivity';
 import { AppState } from '../AppState';
 import { Keep } from '../models/Keep.js';
 import { keepsService } from '../services/KeepsService';
@@ -23,6 +34,7 @@ export default {
     props: { keep: { type: Keep, required: true } },
     setup() {
         return {
+            account: computed(() => AppState.account),
             async setActiveKeep(keepId) {
                 try {
                     AppState.activeKeep = null
@@ -63,5 +75,22 @@ export default {
 
 img:hover {
     box-shadow: 5px 5px 10px rgba(0, 0, 0, 0.25);
+}
+
+.create-options {
+    font-family: 'Oxygen';
+    font-style: normal;
+    font-weight: 700;
+    font-size: 16px;
+    line-height: 20px;
+    color: #2D2D2D;
+}
+
+.dropdown-item:hover {
+    background-color: #d0c8da;
+}
+
+.edit-keep {
+    color: #FFFFFF;
 }
 </style>
--- keepr.client/src/pages/AccountPage.vue
+++ keepr.client/src/pages/AccountPage.vue
@@ -1,25 +1,88 @@
 <template>
-  <div class="about text-center">
-    <h1>Welcome {{ account.name }}</h1>
-    <img class="rounded" :src="account.picture" alt="" />
-    <p>{{ account.email }}</p>
+  <div class="container">
+    <div class="row justify-content-center mt-5">
+      <div class="col-lg-12 d-flex justify-content-center">
+        <div>
+          <img class="cover-image"
+            src="https://i.etsystatic.com/26950383/r/il/d906d6/4272278909/il_1080xN.4272278909_3hrr.jpg" alt="">
+        </div>
+      </div>
+      <div class="col-lg-12 d-flex flex-column align-items-center user-details">
+        <img class="profile-picture rounded-circle" :src="account?.picture" alt="">
+        <div class="mt-3">
+          <h1>{{ account?.name }}</h1>
+        </div>
+        <div>
+          <span>5 vaults</span> | <span>{{ keeps?.length }} keeps</span>
+        </div>
+      </div>
+      <div class="col-lg-10">
+        <h3>Vaults</h3>
+      </div>
+      <div class="col-lg-10">
+        <h3>Keeps</h3>
+        <section class="masonry">
+          <KeepCard v-for="keep in keeps" :keep="keep" />
+        </section>
+      </div>
+    </div>
   </div>
+
+  <Modal id="keep-details">
+    <KeepDetails />
+  </Modal>
 </template>
 
 <script>
 import { computed } from 'vue'
 import { AppState } from '../AppState'
+import KeepCard from '../components/KeepCard.vue';
+import Modal from '../components/Modal.vue';
+import KeepDetails from '../components/KeepDetails.vue';
 export default {
   setup() {
     return {
-      account: computed(() => AppState.account)
-    }
-  }
+      account: computed(() => AppState.account),
+      keeps: computed(() => AppState.keeps.filter(k => k.creatorId == AppState.account.id))
+    };
+  },
+  components: { KeepCard, Modal, KeepDetails }
 }
 </script>
 
-<style scoped>
-img {
-  max-width: 100px;
+<style scoped lang="scss">
+$col-gap: 40px;
+$m-gap: 20px;
+
+.masonry {
+  columns: 201px;
+  column-gap: $col-gap;
+  margin-top: $m-gap;
+
+  &>div {
+    display: inline-block;
+  }
+}
+
+.cover-image {
+  height: 338px;
+  width: 759px;
+  border-radius: 6px;
+}
+
+.profile-picture {
+  height: 150px;
+  width: 150px;
+}
+
+.user-details {
+  transform: translateY(-75px);
+  font-family: 'Oxygen';
+  font-style: normal;
+  font-weight: 400;
+  font-size: 20px;
+  line-height: 25px;
+  text-align: center;
+  color: #000000;
 }
 </style>
